0118a80286c7a913e184c125911e9e5be063937d
qiniuGe <qiniulight@gmail.com>
2024-03-21 14:57:40 +0800

    1

--- NTrSDK.framework/Headers/ManagerTool.h
+++ NTrSDK.framework/Headers/ManagerTool.h
@@ -7,11 +7,13 @@
 
 #import <Foundation/Foundation.h>
 
+//#define NTR_BASE_URL    @"http://192.168.50.64:8099"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ManagerTool : NSObject
-
 + (void)initWithURL:(NSString *)url;
+//+ (instancetype)sharedInstance;
 
 @end
 
